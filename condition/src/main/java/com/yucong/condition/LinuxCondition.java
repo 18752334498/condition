aa12b9891772ace86daadbc3c2d7e4245f122fd9
18752334498 <2424693824@qq.com>
2018-05-22 16:57:11 +0800

    删除注释

--- condition/src/main/java/com/yucong/condition/LinuxCondition.java
+++ condition/src/main/java/com/yucong/condition/LinuxCondition.java
@@ -6,15 +6,6 @@ import org.springframework.core.type.AnnotatedTypeMetadata;
 
 public class LinuxCondition implements Condition {
 
-    /**
-     * 返回true的时候,LinuxCondition这个类才会存在
-     * 通过@Conditional来判断这个类是否存在
-     * 如果存在就会做一些配置
-     *
-     * @param context
-     * @param metadata
-     * @return
-     */
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
         System.out.println("condition......linux.....");
