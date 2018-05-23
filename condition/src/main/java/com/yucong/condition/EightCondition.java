c8882ae4bb3c0b276162659b87af52ca9b1102fb
18752334498 <2424693824@qq.com>
2018-05-23 09:53:18 +0800

    代码修改

--- condition/src/main/java/com/yucong/condition/EightCondition.java
+++ condition/src/main/java/com/yucong/condition/EightCondition.java
@@ -3,12 +3,12 @@ package com.yucong.condition;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
+import org.springframework.web.servlet.DispatcherServlet;
 
 public class EightCondition implements Condition {
 
     @Override
     public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
-        System.out.println(".............");
         return context.getEnvironment().getProperty("server.port").contains("8000");
     }
 }
